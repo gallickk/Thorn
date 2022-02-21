288ada6fa621595ceb09a43b4ec5fd46e21bfc59
Gallickk <aaron@DESKTOP-QRFDM8N>
2022-02-21 00:05:30 +0000

    Premake5 file and batch file to automate project generation.

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Thorn"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+project "Thorn"
+	location "Thorn"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"TH_PLATFORM_WINDOWS",
+			"TH_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "TH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "TH_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "TH_Dist"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Thorn/vendor/spdlog/include",
+		"Thorn/src"
+	}
+
+	links
+	{
+		"Thorn"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"TH_PLATFORM_WINDOWS"
+		}
+
+		filter "configurations:Debug"
+			defines "TH_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "TH_Release"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "TH_Dist"
+			optimize "On"
